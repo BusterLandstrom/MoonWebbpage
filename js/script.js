ebe2101a748af5dc7296de5a969a5d8246f76c5d
ViktorLandstrom <viktor.landstrom@elev.ga.ntig.se>
2020-05-26 12:57:25 +0200

    Added more colors for random color array

--- js/script.js
+++ js/script.js
@@ -15,7 +15,16 @@ var ballX = 0;
 var ballY = 0;
 var yVel = 1;
 var xVel = 1;
-var color = ["blue","red","yellow","pink","purple","green","orange","magenta","brown"]; 
+var color = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF']; 
 var colorRand = "blue";
 var imgWidth = window.innerWidth/8;
 var imgHeight = window.innerHeight/8;
@@ -82,11 +91,11 @@ function update(){
     if (ballX >= WIDTH - 80) {
         ballX = WIDTH - 80;
         xVel = -4;
-        colorRand = color[Math.floor((Math.random() * 10) + 1)];
+        colorRand = color[Math.floor((Math.random() * color.length()) + 1)];
     } else if (ballX <= 0 + 80) {
         ballX = 0 + 80;
         xVel = 4;
-        colorRand = color[Math.floor((Math.random() * 10) + 1)];
+        colorRand = color[Math.floor((Math.random() * color.length()) + 1)];
     }
     /**/
 
@@ -94,11 +103,11 @@ function update(){
     if (ballY >= HEIGHT - 80) {
         ballY = HEIGHT - 80;
         yVel = -1;
-        colorRand = color[Math.floor((Math.random() * 10) + 1)];
+        colorRand = color[Math.floor((Math.random() * color.length()) + 1)];
     } else if (ballY <= 0 + 80) {
         ballY = 0 + 80;
         yVel = 1;
-        colorRand = color[Math.floor((Math.random() * 10) + 1)];
+        colorRand = color[Math.floor((Math.random() * color.length()) + 1)];
     }
     /**/
     createImage(patreonImg, patreonX, patreonY, imgWidth, imgHeight);
